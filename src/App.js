f7ebd000164568ab70bf72cdcad9edc5b5efe00d
Riley-Robinson <rrtoinfinity@gmail.com>
2020-03-27 11:27:27 -0400

    dependecies and app.js good to go

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React from 'react';
+import './App.css';
+import { Route } from "react-router-dom";
+import Home from "./Components/Home.js";
+import Form from "./Components/Form.js";
+
 
 const App = () => {
   return (
-    <>
+    <div classNAme="app">
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <h2>where progammers eat</h2>
+      <br></br>
+      <Route exact path="/" component={Home}/>
+      <Route path="/form" component={Form}/>
+      </div>
+
   );
 };
 export default App;
